f4297482d4980bfe616d4aea2b110a26adb585cc
raj-pandith <rajpandith1945@gmail.com>
2026-04-25 17:19:33 +0530

    modified init

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -8,33 +8,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 
-
-template_dir = os.path.abspath('FlaskWebProject/templates')
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__)
 app.config.from_object(Config)
 app.logger.setLevel(logging.DEBUG)
 
-# Set up detailed logging for debugging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
 streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.DEBUG)  # Changed from WARNING to DEBUG
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-streamHandler.setFormatter(formatter)
+streamHandler.setLevel(logging.WARNING)
 app.logger.addHandler(streamHandler)
 
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
-
-# Add error logging handler
-@app.errorhandler(Exception)
-def log_exception(e):
-    app.logger.exception(f"Unhandled exception: {str(e)}")
-    raise
 
 import FlaskWebProject.views
